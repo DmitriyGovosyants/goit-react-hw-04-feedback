9386a175ef972006cfe55a4fd11264df17692bdf
DmitriyGovosyants <Demiangvs@gmail.com>
2022-07-19 20:18:37 +0300

    refact: change switch break to return

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -16,16 +16,13 @@ export const App = () => {
   const handleIncrement = option => {
     switch (option) {
       case 'good':
-        setGood(s => s + 1);
-        break;
+        return setGood(s => s + 1);
 
       case 'neutral':
-        setNeutral(s => s + 1);
-        break;
+        return setNeutral(s => s + 1);
 
       case 'bad':
-        setBad(s => s + 1);
-        break;
+        return setBad(s => s + 1);
 
       default:
         return;
@@ -54,54 +51,3 @@ export const App = () => {
     </Page>
   );
 };
-
-// export class App extends Component {
-//   state = {
-//     good: 0,
-//     neutral: 0,
-//     bad: 0,
-//   };
-
-//   stateKeys = () => {
-//     return Object.keys(this.state);
-//   };
-
-//   handleIncrement = index => {
-//     const options = this.stateKeys();
-//     const currentBtn = options[index];
-
-//     this.setState(prev => {
-//       return { [currentBtn]: prev[currentBtn] + 1 };
-//     });
-//   };
-
-//   render() {
-//     const { good, neutral, bad } = this.state;
-//     const { handleIncrement, stateKeys } = this;
-//     const options = stateKeys();
-//     const total = countTotalFeedback(good, neutral, bad);
-//     const positive = countPositiveFeedbackPercentage(total, good);
-
-//     return (
-//       <Page>
-//         <Container>
-//           <Section title="Please leave feedback">
-//             <FeedbackOptions
-//               options={options}
-//               onLeaveFeedback={handleIncrement}
-//             />
-//           </Section>
-//           <Section title="Statistics">
-//             <Statistics
-//               good={good}
-//               neutral={neutral}
-//               bad={bad}
-//               total={total}
-//               positivePercentage={positive}
-//             />
-//           </Section>
-//         </Container>
-//       </Page>
-//     );
-//   }
-// }
